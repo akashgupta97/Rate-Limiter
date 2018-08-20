2c9bafa06f4a2c46cd24cfc0d5bba820ac6e52c9
Akash <akash.darknight@gmail.com>
2018-08-20 16:37:00 +0530

    Main_Script

---
+++ blue_final.py
@@ -0,0 +1,11 @@
+#Created By:- Devyansh, Akash, Krishna
+import urllib.parse,urllib.request,urllib.error
+import time
+import os
+import ssl
+import csv
+import json
+import blue_lib
+t = 10
+# if your average internet speed is below 1MBps , 
+#then please increase the numerical in the speed_factor.txt by a step of 2
