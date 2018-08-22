6f6bfabe1542b06347a486e3907c4e50f7c53650
Akash <akash.darknight@gmail.com>
2018-08-22 16:37:00 +0530

    Main_Script

--- blue_final.py
+++ blue_final.py
@@ -1,4 +1,4 @@
-#Created By:- Devyansh, Akash, Krishna
+#Created By:- Akash
 import urllib.parse,urllib.request,urllib.error
 import time
 import os
@@ -9,3 +9,28 @@ import blue_lib
 t = 10
 # if your average internet speed is below 1MBps , 
 #then please increase the numerical in the speed_factor.txt by a step of 2
+blue_lib.clean_slate_protocol()
+try:
+    file = open('speed_factor.txt', 'r')
+    b = file.readline()
+    file.close()
+    t = int(b.rstrip().lstrip())
+except:
+    t = 10
+file = open('pdata.txt', 'a')
+file.flush()
+file.close()
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+try:
+    os.remove('outp.txt')
+except:
+    pass
+try:
+    os.remove('pdata.txt')
+except:
+    pass
+start = time.clock()
+#############start program###############################
