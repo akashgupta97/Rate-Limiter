4a7eb80b70b03f3e542e0461a7e8c315b47a1833
Akash <akash.darknight@gmail.com>
2018-09-08 16:37:00 +0530

    Functions used

--- blue_lib.py
+++ blue_lib.py
@@ -5,3 +5,10 @@ import ssl
 import os
 import urllib.parse,urllib.request,urllib.error
 
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+x = 1
+
+
