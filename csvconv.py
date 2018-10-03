d0ea4554eb816db1b4de1c112ef2c351b53081f1
Akash <akash.darknight@gmail.com>
2018-10-03 16:37:00 +0530

    File for CSV writing

--- csvconv.py
+++ csvconv.py
@@ -3,3 +3,15 @@
 #importing header files
 import os
 import csv
+
+#Initializing parameters
+Main_List=list()   # List storing final values
+Temp_List1=list()
+Temp_List2=list()
+tuple=()
+count=0
+
+i=0  #Temporary variable to traverse through the Main_List
+
+FileName1=open("outp.txt",'r')   #opening file
+
